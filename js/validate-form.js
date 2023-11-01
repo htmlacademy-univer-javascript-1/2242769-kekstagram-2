0d5bc930d900ff67bfc280515981db0b92dda64d
Kolya <ryt9992@mail.ru>
2023-11-01 16:32:05 +0500

    module8-task2

--- js/validate-form.js
+++ js/validate-form.js
@@ -1,51 +1,93 @@
-const MAX_DESCRIPTION_LENGTH = 140;
-const MAX_HASHTAG_LENGTH = 20;
-const MAX_HASHTAGS_COUNT = 5;
-const formUpload = document.querySelector('#upload-select-image');
-const textHashtag = document.querySelector('.text__hashtags');
-const textDescription = document.querySelector('.text__description');
-const hastegValidateSymbols = /^#[a-zа-яё0-9]{1,19}$/;
+const MAX_COMMENT_SYMBOLS = 140;
+const MAX_HASHTAG_SYMBOLS = 20;
+const MAX_HASHTAGS = 5;
 
-const pristine = new Pristine(formUpload, {
-  classTo: 'img-upload__field-wrapper',
-  errorTextParent: 'img-upload__field-wrapper'
+const form = document.querySelector('.img-upload__form');
+const inputHashtag = document.querySelector('.text__hashtags');
+const inputComment = document.querySelector('.text__description');
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__text',
+  errorClass: 'img-upload__text--invalid',
+  successClass: 'img-upload__text--valid',
+  errorTextParent: 'img-upload__text',
+  errorTextTag: 'div',
+  errorTextClass: 'img-upload__error-text',
 });
 
-const validateHashtags = (symbol) => {
-  const hashtagsLower = symbol.toLowerCase();
-  const hashtags = hashtagsLower.split(' ');
-  const uniqueHashtags = [...new Set(hashtags)];
-  if (symbol === '') {
+let errorMessage = '';
+
+const error = () => errorMessage;
+
+const validateHashtag = (value) => {
+  const inputText = value.toLowerCase().trim();
+
+  if (!inputText) {
     return true;
   }
 
-  for (const hashtag of hashtags) {
-    if (!hastegValidateSymbols.test(hashtag)) {
-      return false;
-    }
+  const inputArray = inputText.split(/\s+/);
+
+  if (inputArray.length === 0) {
+    return true;
   }
-  return hashtags.length <= MAX_HASHTAGS_COUNT && hashtags.length === uniqueHashtags.length;
+
+  const rules = [
+    {
+      check: inputArray.some((item) => (item[0] === '#' && item.length === 1)),
+      error: 'Хеш-тег не может состоять только из одной решётки',
+    },
+    {
+      check: inputArray.some((item) => item.indexOf('#', 1) >= 1),
+      error: 'Хэш-теги разделяются пробелами',
+    },
+    {
+      check: inputArray.some((item) => item[0] !== '#'),
+      error: 'Хэш-тег должен начинаться с символа #',
+    },
+    {
+      check: inputArray.some((item, num, array) => array.includes(item, num + 1)),
+      error: 'Хэш-теги не должны повторяться',
+    },
+    {
+      check: inputArray.some((item) => item.length > MAX_HASHTAG_SYMBOLS),
+      error: `Максимальная длина одного хэш-тега ${MAX_HASHTAG_SYMBOLS} символов, включая решётку`,
+    },
+    {
+      check: inputArray.length > MAX_HASHTAGS,
+      error: `Нельзя указать больше ${MAX_HASHTAGS} хэш-тегов`,
+    },
+    {
+      check: inputArray.some((item) => !/^#[a-zа-яё0-9]{1,19}$/i.test(item)),
+      error: 'Хэш-тег содержит недопустимые символы',
+    }
+  ];
+
+  return rules.every((rule) => {
+    const isInvalid = rule.check;
+    if (isInvalid) {
+      errorMessage = rule.error;
+    }
+    return !isInvalid;
+  });
 };
 
-const validateDescriptions = (description) => description.length <= MAX_DESCRIPTION_LENGTH;
+const validateComment = (value) => value.length <= MAX_COMMENT_SYMBOLS;
 
-
-const validateForm = (evt) => {
+const onFormInput = (evt) => {
   if (!pristine.validate()) {
     evt.preventDefault();
   }
 };
 
-pristine.addValidator(
-  textHashtag,
-  validateHashtags,
-  `Хэш-тег начинается с символа #. Нельзя указать больше ${MAX_HASHTAGS_COUNT} хэш-тегов. Максимальная длина одного хэш-тега ${MAX_HASHTAG_LENGTH} символов, включая решётку`
+const resetForm = () => {
+  pristine.reset();
+};
+
+pristine.addValidator(inputHashtag, validateHashtag, error);
+
+pristine.addValidator(inputComment, validateComment,
+  `Длина комментария должна быть не более ${MAX_COMMENT_SYMBOLS} символов`,
 );
 
-pristine.addValidator(
-  textDescription,
-  validateDescriptions,
-  `Длина комментария не может составлять больше ${MAX_DESCRIPTION_LENGTH} символов`
-);
-
-export { validateForm };
+export { onFormInput, resetForm };
