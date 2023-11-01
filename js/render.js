a288a92de0ceb2e2a569caf387c1a29b7d3f8817
Kolya <ryt9992@mail.ru>
2023-11-01 16:07:28 +0500

    module7-task2

--- js/render.js
+++ js/render.js
@@ -1,18 +1,19 @@
 import { arrayObject } from './data.js';
+import { bigPhotoClickHandler } from './big_picture.js';
 
 const pictureTemplate = document.querySelector('#picture').content;
 const newPictureTemplate = pictureTemplate.querySelector('.picture');
 const pictureContainer = document.querySelector('.pictures');
 
-const getPhotoElement = ({ url, likes, comments }) => {
+const getPhotoElement = (photo) => {
   const clonedPicture = newPictureTemplate.cloneNode(true);
-  clonedPicture.querySelector('img').src = url;
-  clonedPicture.querySelector('.picture__likes').textContent = likes;
-  clonedPicture.querySelector('.picture__comments').textContent = comments.length;
+  clonedPicture.querySelector('img').src = photo.url;
+  clonedPicture.querySelector('.picture__likes').textContent = photo.likes;
+  clonedPicture.querySelector('.picture__comments').textContent = photo.comments.length;
+  bigPhotoClickHandler(clonedPicture, photo);
 
   return clonedPicture;
 };
-
 const renderPhotos = (photos) => {
   const fragment = document.createDocumentFragment();
   for (let i = 0; i < photos.length; i++) {
@@ -20,6 +21,5 @@ const renderPhotos = (photos) => {
   }
   pictureContainer.append(fragment);
 };
-
 renderPhotos(arrayObject);
 export { pictureContainer };
